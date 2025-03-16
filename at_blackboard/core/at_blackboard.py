7d3f56c03d8fc3c83871a430005304b4dcaa581a
grigadnal625 <grigandal625@gmail.com>
2025-03-17 00:21:31 +0300

    lint

--- at_blackboard/core/at_blackboard.py
+++ at_blackboard/core/at_blackboard.py
@@ -1,7 +1,12 @@
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import TypedDict
+from typing import Union
+
 from at_queue.core.at_component import ATComponent
 from at_queue.core.session import ConnectionParameters
 from at_queue.utils.decorators import authorized_method
-from typing import TypedDict, Dict, Optional, Union, List
 
 
 class BBItemDict(TypedDict):
@@ -25,7 +30,7 @@ class ATBlackBoard(ATComponent):
 
     @authorized_method
     def set_item(
-        self, 
+        self,
         ref: str,
         value: Optional[Union[str, int, float, bool]] = None,
         belief: Optional[Union[int, float]] = None,
@@ -33,55 +38,61 @@ class ATBlackBoard(ATComponent):
         accuracy: Optional[Union[int, float]] = None,
         **kwargs
     ) -> BBItemDict:
-        dir_key = kwargs.pop('auth_token', 'default')
+        dir_key = kwargs.pop("auth_token", "default")
         dir = self._bb.get(dir_key, {})
-        
+
         if value is None:
             dir.pop(ref, None)
             self._bb[dir_key] = dir
             return self.empty_item
 
         item: BBItemDict = {}
-        item['ref'] = ref
-        item['value'] = value
-        item['belief'] = belief
-        item['probability'] = probability
-        item['accuracy'] = accuracy
+        item["ref"] = ref
+        item["value"] = value
+        item["belief"] = belief
+        item["probability"] = probability
+        item["accuracy"] = accuracy
         item.update(kwargs)
 
         dir[ref] = item
         self._bb[dir_key] = dir
         return item
-    
+
     @authorized_method
     def set_items(self, items: List[BBItemDict], auth_token: str = None) -> List[BBItemDict]:
         return [self.set_item(**item, auth_token=auth_token) for item in items]
-    
+
     @authorized_method
     def get_item(self, ref: str, auth_token: str = None) -> EBBItemDict:
-        dir_key = auth_token or 'default'
+        dir_key = auth_token or "default"
         dir = self._bb.get(dir_key, {})
         item = dir.get(ref)
         if item is None:
             return self.empty_item
         return item
-    
+
     @authorized_method
     def get_items(self, refs: List[str], auth_token: str = None) -> List[EBBItemDict]:
         return [self.get_item(ref, auth_token=auth_token) for ref in refs]
-    
+
     @authorized_method
     def get_all_items(self, auth_token: str = None) -> List[BBItemDict]:
-        dir_key = auth_token or 'default'
+        dir_key = auth_token or "default"
         dir = self._bb.get(dir_key, {})
         return list(dir.values())
+
+    @authorized_method
+    def clear(self, auth_token: str = None):
+        dir_key = auth_token or "default"
+        self._bb[dir_key] = {}
+        return True
 
     @property
     def empty_item(self):
         item: EBBItemDict = {}
-        item['ref'] = None
-        item['value'] = None
-        item['belief'] = None
-        item['probability'] = None
-        item['accuracy'] = None
+        item["ref"] = None
+        item["value"] = None
+        item["belief"] = None
+        item["probability"] = None
+        item["accuracy"] = None
         return item
